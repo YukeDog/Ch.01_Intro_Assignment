64052ee023f4393695831a6964ca0cd10030c5b0
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-25 09:35:41 -0500

    Update Intro_Github_VCS.py

--- Intro_Github_VCS.py
+++ Intro_Github_VCS.py
@@ -1,5 +1,5 @@
 '''
-Each student has 10 lines of code to play with!
+Mr. Hermon will assign each student 10 lines of code.
 For example, student #1 has lines 10-19
 and student #2 has lines 20-29.
 Be careful not to use anyone else's lines.
@@ -7,206 +7,206 @@ Tell me some interesting facts about yourself.
 
 
 
-------------------Matthew Avis------------------
-this is the master branch
-i have no pets
-8/15/06
-i like filipino food
-i longboard during the summer
-i have a brother and sister
-im the youngest
-pool is my favorite sport
-i play drums for worship band
-------------------Andrew Donoho------------------
-this is the master branch
-I like to play video games
-I am on cross country team
-things that I hate to do is math
-The best subject at school is history
-I love to eat apple pie
-i love to eat ice cream
-my favourite game to play is minecraft
-I would love to go to italy
-------------------Owen Earp------------------
-This is the master branch
-favorite color: purple
-02/27/07
-I have 2 sisters
-I live in Urbandale
-I am 15
-I have a dog
-Math is the best class
-I'm hungry
-------------------Tanner Evitts------------------
-This is the master branch
-favorite color: blue
-01/18/2007
-I play Baseball during the summer
-I golf during the fall
-I like to work with computers
-I have a brother named Austen
-Austen's birthday was yesterday
-I have 2 cats
-------------------Matthew Fletcher------------------
-this is the master branch
-10/24/2005
-favorite color: purple
-I play trombone
-My favorite band is acid black cherry
-My favorite video game is destiny
-My least favorite color is yellow
-I'm taking German III
-I'm the middle child
---------------Matthew Hammerand---------------
-This is the master branch
-01/12/2006
-favorite color: orange
-favorite animal: monkey
-favorite food: mac n cheese
-least favorite color: neon pink
-least favorite food: anything spicy
-sports im in: cross country
-language im taking: adv spanish III
+------------------Student 01------------------
+
+
+
+
+
+
+
+
+
+------------------Student 02------------------
+
+
+
+
+
+
+
+
+
+------------------Student 03------------------
+
+
+
+
+
+
+
+
+
+------------------Student 04------------------
+
+
+
+
+
+
+
+
+
+------------------Student 05------------------
+
+
+
+
+
+
+
+
+
+------------------Student 06------------------
+
+
+
+
+
+
+
+
+
 ------------------Student 07------------------
-Oded H
-This is the master branch
-09/2006
-favorite color: any
-I run Cross Country
-I am in Band
-I like STEM Related Classes
-Tommith sits next to me in CSP
-He calls me Odedulous
-------------------Tommy Hensley------------------
-This is the master branch
-favorite color: Red
-08/26/05
-I run Track and Cross Country
-I sit near Odedulous
-Oded Calls me Tomith
-I have Math after this
-We have a meet Saturday
-This is my last line
-+!#+-*-=^_*+-_-*Ethan Huynh-_^-*-+=*=!+^_*^=+
-I play games sometimes
-10/30/04
-my favorite food is pasta
-my favorite color is red
-I work at hyvee but I want to work at smokey row
-I have a caffeine addiction
-I like star wars too
-I drive a hatchback impreza
-I listen to music a lot
-------------------Lindy Huynh------------------
-I like playing video games.
-I'm currently playing Silent Hill 3 and multiple FPS games.
-My list of retired favorites is too long to list here because I have hundreds of great games in my Steam library.
-I also like reading. I am currently reading 1984 by George Orwell.
-I enjoy taking brief walks, especially when the weather is perfectly cold and mildly breezy.
-I enjoy exercising, mainly weightlifting and running.
-One of my favorite hobbies I wish I didn't have was laying in bed and listening to music while letting my mind wander.
-My last meal would be a nice rare steak with a lobster tail and a glass of [REDACTED]. Some fruit wouldn't hurt either.
-What color are you?
-------------------Christian KElly------------------
-This is the master Branch
-favorite color: purple
-04/07/2006
-favorite sport: soccer
-Favorite food: Burger
-Favorite subject: Science
-Favorite season: winter
-favorite State: California
-Favorite class: Strength
-------------------Arianna Lear------------------
-This is the Master branch
-Favorite color: blue
-01/18/2006
-Do I have pets? yes
-Favorite Animal: Horse
-Favorite core subject: Language Arts
-Busiest club I'm part of: Theater
-favorite place to travel: Chicago
-Ever been out of state? Yes
-------------------Anna Legler------------------
-This is the master branch
-My birthday is May 9th 2006
-My favorite color is blue
-I have a dog, she is a goldendoodle
-I have two sisters
-My favorite food is pasta
-My least favorite food is bananas
-I moved to Iowa when I was 2
-I play trumpet and piano
-------------------Rachel Linthicum------------------
-This is the master branch
-Date of Birth: 10/06/06
-Favorite Color: yellow
-Favorite Movie Franchise: MCU
-Favorite Animal: Monkey
-Favorite Book Author: Rick Riordan
-Favorite Season: Fall/Autumn
-Favorite Sports: Volleyball & Basketball
-Favorite Month: October
-------------------Carter Mcinville------------------
-This is the master branch
-favorite color: pink
-07/01/2005
-I play baseball
-cars 2 was a disaster
-Better call saul is top 3 series of all time
-i have renegade raider
-kanye west is consistently in my playlist
-Texas roadhouse makes some pretty good rolls
-------------------Shea Hermon------------------
-This Is The Master Branch
-Shea Hermon
-Favorite Color Is Red
-I like to Fish
-I have red hair
-I like Origammi
-I like football
-I like pizza
-I like To Code
-------------------Benjamin------------------
-This is the master branch
-06/24/2005
-favorite color is blue
-I play soccer for the school
-My favorite fast food chain is Chick Fil A
-I can speak 3 launguages
-I don't have a car
-I am broke
-I like food
-------------------Carter Sousa------------------
-This is the master branch
-Favorite color: Maroon and Teal
-Birthday: 12/28/2004
-fear:Darkness
-favorite crustacean: Mantis Shrimp
-favorite insect: Dragonfly
-Favorite mammal: Maned Wolf
-Favorite conversation topics: Kardashev Scale
-Hobbies: Soccer, Cooking, Sewing, Rock Climbing
-------------------Kadin Terronez------------------
-this is the master branch
-favorite color: blue
-06/06/2006
-favorite animal: dog
-favorite sport: baseball
-favorite food: steak
-favorite subject: math
-favorite app: Youtube
-favorite class: Adv. Accounting
-------------------Aidan Z.------------------
-This is the Master Branch
-fav color blue
-04/01/05
-I have played hockey for 10 years
-I want to go into aerospace/astronautical engineering
-Is water wet?
-Who is Shrodinger?
-Why do I have is cat?
-Wait, where did the cat go?
+
+
+
+
+
+
+
+
+
+------------------Student 08------------------
+
+
+
+
+
+
+
+
+
+------------------Student 09------------------
+
+
+
+
+
+
+
+
+
+------------------Student 10------------------
+
+
+
+
+
+
+
+
+
+------------------Student 11------------------
+
+
+
+
+
+
+
+
+
+------------------Student 12------------------
+
+
+
+
+
+
+
+
+
+------------------Student 13------------------
+
+
+
+
+
+
+
+
+
+
+------------------Student 14------------------
+
+
+
+
+
+
+
+
+------------------Student 15------------------
+
+
+
+
+
+
+
+
+
+------------------Student 16------------------
+
+
+
+
+
+
+
+
+
+------------------Student 17------------------
+
+
+
+
+
+
+
+
+
+------------------Student 18------------------
+
+
+
+
+
+
+
+
+
+------------------Student 19------------------
+
+
+
+
+
+
+
+
+
+------------------Student 20------------------
+
+
+
+
+
+
+
+
+
 ------------------Student 21------------------
 
 
@@ -248,6 +248,61 @@ Wait, where did the cat go?
 
 
 ------------------Student 25------------------
+
+
+
+
+
+
+
+
+
+------------------Student 26------------------
+
+
+
+
+
+
+
+
+
+------------------Student 27------------------
+
+
+
+
+
+
+
+
+
+------------------Student 28------------------
+
+
+
+
+
+
+
+
+
+------------------Student 29------------------
+
+
+
+
+
+
+
+
+
+------------------Student 30------------------
+
+
+
+
+
 
 
 
